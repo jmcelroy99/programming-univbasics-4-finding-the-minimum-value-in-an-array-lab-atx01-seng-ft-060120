74b2d6833eaa5f94aee0a8cc60b733c6ea71da9b
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-17 17:15:59 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,14 @@
 def find_min_value(array)
   # Add your solution here
+  count = 0
+  minimum_num = 100,000
+  
+  while count < array.length do
+    if array[count] < minimum_num
+      minimum_num = array[count]
+    end
+    
+    count += 1
+  end
+  minimum_num
 end
